a48ffc8ff102a78ca8a836cbce12dffc867454c9
chwebdev815 <xandokan815@gmail.com>
2021-08-18 14:42:07 +0800

    completed login functionality

--- Designs/freddy-dashboard/src/components/login.component.js
+++ Designs/freddy-dashboard/src/components/login.component.js
@@ -1,11 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import logoIcon from "../image/Freddys_Logo.svg";
+import { useHistory } from 'react-router-dom';
 
 export const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState(null);
+  const history = useHistory();
+
+  const handleUsernameChange = e => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = e => {
+    setPassword(e.target.value);
+  };
+
   const handleSubmit = event => {
     event.preventDefault();
-    console.log('called');
-  }
+    setErrorMessage(null);
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        username: username,
+        password: password
+      })
+    };
+    fetch('https://freddy.codesubmit.io/login', requestOptions)
+      .then(response => response.json())
+      .then(data => {
+        if (data.msg) {
+          console.log(data.msg);
+          setErrorMessage(data.msg);
+        } else {
+          console.log('success login');
+          localStorage.setItem('access_token', data.access_token);
+          history.push('/dashboard');
+        }
+      })
+  };
+
   return (
     <form className="login-form" onSubmit={handleSubmit}>
       <div className="logo-section">
@@ -14,13 +49,13 @@ export const Login = () => {
       </div>
 
       <div className="form-group">
-        <input type="email" className="form-control" placeholder="username" />
+        <input type="text" className="form-control" placeholder="username" value={username} onChange={handleUsernameChange}/>
       </div>
 
       <div className="form-group">
-        <input type="password" className="form-control" placeholder="Enter password" />
+        <input type="password" className="form-control" placeholder="Enter password" value={password} onChange={handlePasswordChange} />
       </div>
-
+      {errorMessage && <p>{errorMessage}</p>}
       <button type="submit" className="btn btn-primary btn-block">Login</button>
     </form>
   );
